ebb2ffc2b06525a15536b25b3d1d475d922e64e8
Alex Tareshawty <atareshawty@gmail.com>
2016-04-04 15:52:51 -0400

    Added view for a successful challenge and fake challenge creation

--- controllers/Challenges.js
+++ controllers/Challenges.js
@@ -1,4 +1,5 @@
 const Challenge = require('../models').Challenge;
+const User = require('../models').User;
 
 function ChallengesController() {
   this.showAll = (req, res) => {
@@ -9,9 +10,26 @@ function ChallengesController() {
   };
 
   this.new = (req, res) => {
-    //TODO create new challenges
-    console.log(req.body['challenge-form']);
-    res.send(`You challenged: ${req.body['challenge-form']}. Good luck with that one...`);
+    //TODO create new challenge for req.user
+    const rowFileRegex = /[a-zA-Z]+|[0-9]+/g;
+    const spot = req.body['challenge-form'].match(rowFileRegex);
+    const row = spot[0];
+    const file = spot[1];
+    console.log(`${row} ${file}`);
+    const challengee = User.findOne({
+      where: {
+        row,
+        file
+      }
+    });
+
+    challengee.then((person) => {
+      res.render('challengeCreated', {
+        challengeeName: person.name,
+        row: person.row,
+        file: person.file
+      });
+    });
   };
 }
 
