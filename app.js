24f51989b0cb85f65d39410b86d53293419108b9
Alex Tareshawty <atareshawty@gmail.com>
2016-04-07 10:39:56 -0400

    Added nginx.conf.erb file to set up config for osumbchallenges.dev
    * Added nginx.conf.erb file to set up config for osumbchallenges.dev
    
    * Fixed unit tests
    
    * Updated README
    
    * Rename NGINX.md to README.md
    
    * Modified config readme instructions slightly
    
    * Updated Brewfile. Thanks @tarebyte

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-const config = require('./config');
+const config = require('./config/config');
 const http = require('http');
 
 const routes = require('./routes/routes');
@@ -78,7 +78,7 @@ function start() {
   var port = config.server.port;
   server = http.createServer(app);
   server.listen(port);
-  console.log('Express server listening on port %d in %s mode', port, app.settings.env);
+  console.log('Express server listening on port %d in %s mode', port);
 }
 
 function end() {
