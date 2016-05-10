b66fbeadb443220ee2ac494008eba9e81bd00e6f
Alex Tareshawty <atareshawty@gmail.com>
2016-05-09 21:06:42 -0400

    Added mock data script and npm command to run it

--- models/setup.js
+++ models/setup.js
@@ -7,11 +7,9 @@ models.sequelize.sync({force: true}).then(() => {
   models.Performance.create(config.test.mockPerformance);
   models.Spot.bulkCreate(mockData.getSpotsFromExcelFile(config.db.fakeSpotDataPath))
     .then(() => {
-      console.log('Added Spots');
       models.User.bulkCreate(mockData.getUsersFromExcelFile(config.db.fakeUserDataPath)).then(() => {
-        console.log('Added fake data');
-        models.Challenger.bulkCreate(mockData.getMockChallengesList()).then(() => {console.log('Added fake challengers');});
-        models.Result.bulkCreate(mockData.getFakeResults()).then(() => {console.log('Added fake results');});
+        models.Challenger.bulkCreate(mockData.getMockChallengesList()).then(() => {});
+        models.Result.bulkCreate(mockData.getFakeResults()).then(() => {});
       });
     });
 });
