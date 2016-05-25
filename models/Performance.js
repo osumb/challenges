88bfbed54375fd4c98c1f9a07daf7f01179a95ac
Alex Tareshawty <atareshawty@gmail.com>
2016-05-24 23:38:06 -0400

    Fix static pages controller

--- models/Performance.js
+++ models/Performance.js
@@ -1,3 +1,35 @@
-module.exports = class Performance {
+const moment = require('moment');
 
+const utils = require('../utils');
+
+module.exports = class Performance {
+  getNext() {
+    return new Promise((resolve, reject) => {
+      const client = utils.db.createClient();
+      const queryString = 'SELECT * FROM performances WHERE openAt < $1::date ORDER BY openAt desc LIMIT 1';
+
+      client.connect();
+      client.on('error', (err) => reject(err));
+
+      const query = client.query(queryString, [new Date().toJSON()]);
+
+      query.on('row', (result) => {
+        client.end();
+        resolve(result);
+      });
+
+      query.on('error', (err) => {
+        client.end();
+        reject(err);
+      });
+    });
+  }
+
+  formatPerformance(performance, formatString) {
+    const now = new Date().toJSON();
+    performance.windowOpen = moment(performance.openat).isBefore(moment(now)) && moment(now).isBefore(moment(performance.closeat));
+    performance.openAt = moment(performance.openat).format(formatString);
+    performance.closeAt = moment(performance.closeat).format(formatString);
+    return performance;
+  }
 };
