5bacca816753ff0fd20f92f28136ccee6888ee3b
Alex Tareshawty <atareshawty@gmail.com>
2016-07-09 21:38:48 -0400

    sql function for switch user spots based on challenge results

--- db/schema.sql
+++ db/schema.sql
@@ -34,6 +34,76 @@ BEGIN
 
   UPDATE performances SET current = FALSE WHERE id <> performanceId;
   UPDATE performances SET current = TRUE WHERE id = performanceId;
+
+/*
+  This function assumes that the array of resultIds has been sorted in a special order
+    - Regular vs Alternate
+    - Alternate vs Alternate
+    - Regular vs Regular
+  We're not going to worry about results where only one person is involved
+*/
+DROP FUNCTION IF EXISTS switch_spots_based_on_results(resultIds int[]);
+CREATE OR REPLACE FUNCTION switch_spots_based_on_results(resultIds int[])
+RETURNS VOID AS $$
+DECLARE userOne varchar(256); userTwo varchar(256); winner varchar(256); spotOne char(3); spotTwo char(3);
+winnerSpot char(3); id int; userOneAlternate boolean; userTwoAlternate boolean; spotOpen boolean;
+BEGIN
+
+  FOREACH id IN ARRAY resultIds
+  LOOP
+    SELECT firstNameNumber, secondNameNumber, winnerId, spotId
+    INTO userOne, userTwo, winner, winnerSpot
+    FROM results
+    WHERE result.id = id;
+
+    SELECT spotId, alternate INTO spotOne, userOneAlternate FROM users WHERE nameNumber = userOne;
+    SELECT spotId, alternate INTO spotTwo, userTwoAlternate FROM users WHERE nameNumber = userTwo;
+    SELECT open INTO spotOpen FROM spots WHERE id = spotId;
+
+    -- If it's an alternate vs regular for a non open spot, easy peasy :D
+    IF (userOneAlternate AND NOT userTwoAlternate AND NOT spotOpen) OR
+       (NOT userOneAlternate AND userTwoAlternate AND NOT spotOpen)
+    THEN
+      -- If the userOne won, but wasn't already in the spot won
+      IF userOne = winner AND spotOne <> winnerSpot THEN
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userOne;
+        UPDATE users SET spotId = spotOne WHERE nameNumber = userTwo;
+      ELSIF winner = userTwo AND spotTwo <> winnerSpot THEN
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userTwo;
+        UPDATE users SET spotId = spotTwo WHERE nameNumber = userOne;
+      END IF;
+    END IF;
+
+    -- If two regulars were involved in the challenge
+    IF (userOneAlternate AND userTwoAlternate) THEN
+      IF userOne = winner THEN
+        -- The person who originally had the open spot gets the loser's spot
+        UPDATE users SET spotId = spotTwo WHERE spotId = winnerSpot;
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userOne;
+      ELSE
+        -- The person who originally had the open spot gets the loser's spot
+        UPDATE users SET spotId = spotOne WHERE spotId = winnerSpot;
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userTwo;
+      END IF;
+    END IF;
+
+    -- If two regulars were involved in the challenge
+    IF (NOT userOneAlternate AND NOT userTwoAlternate) THEN
+      IF userOne = winner AND spotOne <> winnerSpot THEN
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userOne;
+        UPDATE users SET spotId = spotOne WHERE nameNumber = userTwo;
+      ELSIF userTwo = winner AND spotTwo <> winnerSpot THEN
+        UPDATE users SET spotId = winnerSpot WHERE nameNumber = userTwo;
+        UPDATE users SET spotId = spotTwo WHERE nameNumber = userOne;
+      END IF;
+    END IF;
+
+  END LOOP;
+
+  -- Set all things back to normal
+  UPDATE spots SET challengedCount = 0, open = FALSE;
+  UPDATE users SET eligible = TRUE WHERE alternate;
+  UPDATE users SET eligible = FALSE WHERE NOT alternate;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -237,7 +307,7 @@ CREATE TABLE results (
   performanceId integer references performances(id) NOT NULL,
 	spotId varchar(3) references spots(id) NOT NULL,
   firstNameNumber varchar(256) references users(nameNumber) NOT NULL,
-	secondNameNumber varchar(256) references users(nameNumber) NOT NULL,
+	secondNameNumber varchar(256) references users(nameNumber),
   firstComments text NOT NULL DEFAULT '',
 	secondComments text NOT NULL DEFAULT '',
 	winnerId varchar(256) references users(nameNumber),
