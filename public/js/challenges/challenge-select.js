93ee7edbe97a50dd1a01ffa94e73f5621c362479
Alex Tareshawty <atareshawty@gmail.com>
2016-07-22 23:30:39 -0400

    Frontend changes for client side new challenge request

---
+++ public/js/challenges/challenge-select.js
@@ -0,0 +1,55 @@
+const selectClass = '.ChallengeSelect';
+const submitButtonClass = '.ChallengeSelect-submit';
+
+$(submitButtonClass).on('click', () => {
+  const optionVals = $(selectClass).val().split(' ');
+  const spotId = optionVals[0], name = optionVals[1];
+
+  $(submitButtonClass).prop('disabled', true);
+  fetch(endPointUrl(), {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'same-origin',
+    method: 'post',
+    body: JSON.stringify({
+      spotId
+    })
+  })
+  .then((response) => response.json())
+  .then(({ code }) => {
+    if (code > 0) {
+      throw code;
+    }
+    return;
+  })
+  .then(() => {
+    banner(`You are challenging ${name} for spot ${spotId}`);
+  })
+  .catch((errorCode) => {
+    let errorMessage;
+
+    /* eslint-disable indent */
+    switch (errorCode) {
+      case 1:
+        errorMessage = 'Sorry! That person was just challenged. Pick another person';
+        break;
+      case 2:
+        errorMessage = 'You\'ve already made a challenge';
+        break;
+      default:
+        errorMessage = 'Sorry! There was an error on our end. We\'re working on the issue';
+    }
+
+    $(submitButtonClass).prop('disabled', false);
+    banner(errorMessage);
+  });
+});
+
+const banner = (message) => {
+  $('.bannerMessage').remove();
+  $('.navbar').after(`<h3 class="bannerMessage">${message}</h3>`);
+};
+
+const endPointUrl = () => `${window.location.pathname.split('/').splice(0, 3).join('/')}/challenges`;
