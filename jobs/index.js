e4a1ac75f47949b1328fc86f10e709fa4f9fe266
Alex Tareshawty <atareshawty@gmail.com>
2016-07-22 21:48:50 -0400

    Jobs: js short hand and TODO comment

--- jobs/index.js
+++ jobs/index.js
@@ -2,15 +2,12 @@ const models = require('../models');
 const Challenge = new models.Challenge();
 const { sendChallengeList } = require('../utils').email;
 
-const toCSV = challenges => {
-  return challenges.map(({ challengee, challenger, challengeespot, challengerspot, spotopen }) => {
-    const challengeeString = spotopen ? 'Open Spot' : challengee;
+const toCSV = (challenges) =>
+  challenges.map(({ challengee, challenger, challengeespot, challengerspot, spotopen }) =>
+    [challengerspot, challenger, challengeespot, spotopen ? 'Open Spot' : challengee]
+  );
 
-    return [challengerspot, challenger, challengeespot, challengeeString];
-  });
-};
-
-const getChallengeCSV = performanceId =>
+const getChallengeCSV = (performanceId) =>
   Challenge.findAllForPerformanceCSV(performanceId)
   .then(challengeList =>
     new Buffer(toCSV(challengeList).reduce((prev, curr) =>
@@ -21,7 +18,7 @@ const getChallengeCSV = performanceId =>
 const sendChallengeListEmail = (performanceId) => {
   getChallengeCSV(performanceId)
   .then(csv => {
-    sendChallengeList('atareshawty@gmail.com', csv)
+    sendChallengeList('atareshawty@gmail.com', csv) // TODO: Actually email to band office
     .catch(err => console.error(err));
   })
   .catch(err => console.error(err));
