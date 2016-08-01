6851869be7dde2117f7f3dac633e79f0a9d12ce5
Alex Tareshawty <atareshawty@gmail.com>
2016-07-31 22:24:33 -0400

    Utils: using papertrail now. Only need to log to console

--- utils/logger.js
+++ utils/logger.js
@@ -1,37 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-
 const { email } = require('./index');
-const timestamp = () => `${new Date().toISOString()} (UTC)`;
 
 const challengesLog = (message) => {
-  fs.appendFile(path.resolve(__dirname, '../logs/challenges.log'), `\n[${timestamp()}]: ${message}`, (err) => {
-    if (err) {
-      console.error('CHALLENGES_LOG_ERROR:', err);
-    }
-  });
+  console.log(`CHALLENGES_LOG: ${message}`);
 };
 
 const errorLog = ({ level, message }) => {
   if (level > 3) {
     email.sendErrorEmail(message);
   }
-  fs.appendFile(
-    path.resolve(__dirname, '../logs/errors.log'),
-    `\n[${timestamp()}]: Level ${level} error: ${message}`,
-    (err) => {
-      if (err) {
-        console.error('CHALLENGES_LOG_ERROR:', err);
-      }
-    });
+  console.error(`ERROR_LOG: LEVEL: ${level}, msg: ${message}`);
 };
 
 const jobsLog = (message) => {
-  fs.appendFile(path.resolve(__dirname, '../logs/jobs.log'), `\n[${timestamp()}]: ${message}`, (err) => {
-    if (err) {
-      console.error('CHALLENGES_LOG_ERROR:', err);
-    }
-  });
+  console.log(`JOBS_LOG: ${message}`);
 };
 
 module.exports = {
