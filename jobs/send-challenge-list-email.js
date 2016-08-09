3d9190cc73c715d383beb5a1dc71ade215c60570
Alex Tareshawty <atareshawty@gmail.com>
2016-08-09 11:08:31 -0400

    Bake user actions into csv export

--- jobs/send-challenge-list-email.js
+++ jobs/send-challenge-list-email.js
@@ -1,32 +1,45 @@
 const models = require('../models');
 const Challenge = new models.Challenge();
+const Manage = new models.Manage();
 const { sendChallengeList } = require('../utils').email;
 const { logger } = require('../utils');
 
-const toCSV = (challenges) =>
+const challengesToCSV = (challenges) =>
   challenges.map(({ challengee, challenger, challengeespot, challengerspot, spotopen }) =>
     [challengerspot, challenger, challengeespot, spotopen ? 'Open Spot' : challengee]
   );
 
+const disciplineTOCSV = (discipline) =>
+  discipline.map(({ name, reason, spotid }) =>
+    [spotid, name, reason]
+  );
+
 const getChallengeCSV = (performanceId) =>
-  Challenge.findAllForPerformanceCSV(performanceId)
-  .then(challengeList =>
-    new Buffer(toCSV(challengeList).reduce((prev, curr) =>
-      `${prev}\n${curr.toString()}`, `OSUMB Challenges\nChallenges`)).toString('base64')
-  )
+  Promise.all([Challenge.findAllForPerformanceCSV(performanceId), Manage.findAllForPerformanceCSV(performanceId)])
+  .then((data) => {
+    const challenges = challengesToCSV(data[0]).reduce((prev, curr) => `${prev}\n${curr.toString()}`, '');
+    const discipline = disciplineTOCSV(data[1]).reduce((prev, curr) => `${prev}\n${curr.toString()}`, '');
+
+    return new Buffer(`OSUMB Challenges\nChallenges\n${challenges}\n\nOpen Spots/Automatic Alternates\n${discipline}`).toString('base64');
+  })
   .then(challengeList => challengeList);
 
 const sendChallengeListEmail = (performanceId) => {
-  getChallengeCSV(performanceId)
+  return getChallengeCSV(performanceId)
   .then(csv => {
     sendChallengeList('atareshawty@gmail.com', csv) // TODO: Actually email to band office
+    .then((data) => {
+      if (data.statusCode > 300) {
+        throw data;
+      }
+      logger.jobsLog('Send Challenge List Email success', data);
+    })
     .catch((err) => {
-      logger.errorLog(`Jobs.sendChallengeListEmail ${err}`);
+      logger.errorLog('Jobs.sendChallengeListEmail', err);
     });
-    logger.jobsLog('Send Challenge List Email success');
   })
   .catch((err) => {
-    logger.errorLog(`Jobs.sendChallengeListEmail ${err}`);
+    logger.errorLog('Jobs.sendChallengeListEmail', err);
   });
 };
 
--- models/Manage.js
+++ models/Manage.js
@@ -36,4 +36,35 @@ module.exports = class Manage {
       });
     });
   }
+
+  findAllForPerformanceCSV(performanceId) {
+    return new Promise((resolve, reject) => {
+      const client = db.createClient();
+      const sql = `SELECT u.name, u.spotid, m.reason
+      FROM manage AS m JOIN users AS u ON m.usernamenumber = u.namenumber
+      WHERE m.performanceId = $1
+      ORDER BY (substring(u.spotid, 0, 2), substring(u.spotid, 2)::int)`;
+      const actions = [];
+
+      client.connect();
+      client.on('error', (err) => reject(err));
+
+      const query = client.query(sql, [performanceId]);
+
+      query.on('row', (action) => {
+        actions.push(action);
+      });
+
+      query.on('end', () => {
+        client.end();
+        resolve(actions);
+      });
+
+      query.on('error', (err) => {
+        client.end();
+        reject(err);
+      });
+    });
+  }
+
 };
