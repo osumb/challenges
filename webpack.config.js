38d970fcfb969662479f8eeb4ea89db688f13ad6
Alex Tareshawty <atareshawty@gmail.com>
2016-08-10 22:36:29 -0400

    Update package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ var webpack = require('webpack');
 
 module.exports = {
   context: path.join(__dirname, 'public'),
-  entry: './stylesheets/style.scss',
+  entry: './index.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'style.css'
+    filename: 'bundle.js'
   },
 
   module: {
@@ -18,11 +18,29 @@ module.exports = {
       {
         test: /\.s?css$/,
         loader: ExtractTextPlugin.extract('style', 'css', 'autoprefixer', 'sass')
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   },
 
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false}
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new webpack.ProvidePlugin({
+      fetch: 'whatwg-fetch'
+    })
   ]
 };
