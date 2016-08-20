553dd0f17a6e8f47855a814e58b7abc8b9254b2f
Alex Tareshawty <atareshawty@gmail.com>
2016-08-20 16:49:05 -0400

    Redirect user to profile page if already logged in. Otherwise, show login form

--- controllers/StaticPages.js
+++ controllers/StaticPages.js
@@ -1,26 +1,16 @@
-const models = require('../models');
-const { logger } = require('../utils');
-const Performance = new models.Performance();
-
 function StaticPagesController() {
   this.home = (req, res) => {
-    let message;
+    if (req.user) {
+      res.redirect(`/${req.user.nameNumber}`);
+    } else {
+      let message;
 
-    if (req.query.auth === 'false') {
-      message = '**Username or password is incorrect**';
+      if (req.query.auth === 'false') {
+        message = '**Username or password is incorrect**';
+      }
+
+      res.render('static-pages/home', { message });
     }
-
-    Performance.findNextOrOpenWindow()
-      .then((performance) => res.render('static-pages/home',
-        {
-          message,
-          user: req.user,
-          performance: Performance.format(performance, 'MMMM Do, h:mm:ss a')
-        }))
-      .catch((err) => {
-        logger.errorLog('StaticPages.home', err);
-        res.render('static-pages/error', { user: req.user });
-      });
   };
 }
 
