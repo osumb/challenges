ca0acd0101365fe707e35fdcc676f1de5100667e
Alex Tareshawty <atareshawty@gmail.com>
2016-08-20 00:06:02 -0400

    Get performance date for result and parse

--- db/queries/results-query.sql
+++ db/queries/results-query.sql
@@ -1,7 +1,15 @@
 SELECT * FROM
-  (SELECT R1.id, get_user_result_comments(R1.firstNameNumber, R1.firstComments, R1.secondComments, $1) AS comments, P.name, R1.winnerId, R1.spotId FROM Results AS R1, Performances AS P
-  WHERE (R1.firstNameNumber = $1 OR R1.secondNameNumber = $1) AND
-  R1.performanceId = P.id AND NOT R1.pending) t1
+  (
+    SELECT
+      R1.id,
+      get_user_result_comments(R1.firstNameNumber, R1.firstComments, R1.secondComments, $1) AS comments,
+      P.name,
+      R1.winnerId,
+      R1.spotId,
+      P.performdate
+    FROM Results AS R1, Performances AS P
+    WHERE (R1.firstNameNumber = $1 OR R1.secondNameNumber = $1) AND R1.performanceId = P.id AND NOT R1.pending
+  ) t1
 LEFT JOIN
   (SELECT U.name AS opponentName, R2.id
   FROM Users AS U, Results AS R2
