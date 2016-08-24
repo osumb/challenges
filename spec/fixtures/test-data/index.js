414de5ecb671e7d05101fc6e7671abd51460c44c
Alex Tareshawty <atareshawty@gmail.com>
2016-08-23 22:41:01 -0400

    Test manage actions

--- spec/fixtures/test-data/index.js
+++ spec/fixtures/test-data/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   testChallenges: require('./test-challenges'),
+  testManageActions: require('./test-manage-actions'),
   testPerformance: require('./test-performance'),
   testSpots: require('./test-spots'),
   testUsers: require('./test-users')
---
+++ spec/fixtures/test-data/test-manage-actions.js
@@ -0,0 +1,1 @@
+module.exports = [{"performanceid":1,"userNameNumber":"Nivens.576","reason":"Failed Music Check","spotId":"F3","voluntary",false}];
