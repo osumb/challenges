ca5bdfe8a3013312115867307c95e901b0bbc646
Alex Tareshawty <atareshawty@gmail.com>
2016-08-28 12:02:42 -0400

    Add middleware for first user login

--- auth/index.js
+++ auth/index.js
@@ -70,10 +70,24 @@ function ensureEvalAbility(req, res, next) {
     return res.redirect('/');
   }
 }
+
+function ensureNotFirstLogin(req, res, next) {
+  console.log('Ensure Not First Login', req.user);
+  if (req.user.new) {
+    return res.render('users/settings', {
+      message: 'This is your first time logging in. Please make a new password',
+      user: req.user
+    });
+  } else {
+    return next();
+  }
+}
+
 module.exports = {
   ensureAuthenticated,
   ensureAdmin,
   ensureAuthAndNameNumberRoute,
   ensureEvalAbility,
+  ensureNotFirstLogin,
   passport
 };
