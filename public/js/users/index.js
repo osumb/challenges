26722a540d298d00ab576bfb75e54fb8a37e9ac0
Alex Tareshawty <atareshawty@gmail.com>
2016-08-28 00:50:35 -0400

    Front end for change password

--- public/js/users/index.js
+++ public/js/users/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable global-require */
 module.exports = {
   individualManage: require('./individual-manage.js'),
-  manage: require('./manage.js')
+  manage: require('./manage.js'),
+  settings: require('./settings.js')
 };
