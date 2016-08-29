8d104ddc144afac3c90fb966f75ff169be47e478
Alex Tareshawty <atareshawty@gmail.com>
2016-08-28 23:07:23 -0400

    Test spot switches

---
+++ spec/6-approve-results-and-switch-spots-spec.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+const { testData } = require('../spec/fixtures');
+const models = require('../models');
+
+const Result = new models.Result();
+const User = new models.User();
+const { testFinalSpots, testPerformance, testResults } = testData;
+
+describe('Approve Results', () => {
+  it('Should approve the results', (done) => {
+    Result.approve([...Array(testResults.length + 1).keys()])
+    .then(() => {
+      done();
+    });
+  });
+});
+
+describe('Switch Spots', () => {
+  it('Should switch spots', (done) => {
+    Result.switchSpotsForPerformance(testPerformance.id)
+    .then(() => {
+      done();
+    });
+  });
+
+  testFinalSpots.forEach(({ nameNumber, spotId }) => {
+    it('Should switch spots correctly', (done) => {
+      User.findByNameNumber(nameNumber)
+      .then(({ spotid }) => {
+        expect(spotid).toEqual(spotId);
+        done();
+      });
+    });
+  });
+});
