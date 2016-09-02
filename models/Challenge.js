1d6f3f765d135fa6d5350e53886f74cee370b8ba
Alex Tareshawty <atareshawty@gmail.com>
2016-09-02 01:32:56 -0400

    Design Challenge model better

--- models/Challenge.js
+++ models/Challenge.js
@@ -5,19 +5,25 @@ const attributes = ['id', 'performanceId', 'userNameNumber', 'spotId'];
 
 class Challenge {
 
-  static getAttributes() {
+  constructor(performanceId, performanceName, spotId) {
+    this._performanceId = performanceId;
+    this._performanceName = performanceName;
+    this._spotId = spotId;
+  }
+
+  static get attributes() {
     return attributes;
   }
 
-  static getIdName() {
+  static get idName() {
     return 'id';
   }
 
-  static getTableName() {
+  static get tableName() {
     return 'challenges';
   }
 
-  create(userId, spotId, performanceId) {
+  static create(userId, spotId, performanceId) {
     const sql = 'SELECT make_challenge($1, $2, $3)';
 
     return new Promise((resolve, reject) => {
@@ -32,10 +38,12 @@ class Challenge {
       query.on('row', ({ make_challenge }) => {
         returnCode = parseInt(make_challenge, 10);
       });
+
       query.on('end', () => {
         client.end();
         resolve(returnCode);
       });
+
       query.on('error', (err) => {
         client.end();
         reject(err);
@@ -43,7 +51,7 @@ class Challenge {
     });
   }
 
-  findAllChallengeablePeopleForUser(user, performanceId) {
+  static findAllChallengeablePeopleForUser(user, performanceId) {
     // eslint-disable-next-line
     const sql = queries.challengeablePeople;
 
@@ -52,6 +60,7 @@ class Challenge {
         resolve(null);
       }
 
+      console.log(user.instrument, user.part, user.spotId, user.nameNumber);
       const client = utils.db.createClient();
       const users = [];
 
@@ -60,16 +69,20 @@ class Challenge {
 
       const query = client.query(sql, [user.instrument, user.part, user.spotId, user.nameNumber, performanceId]);
 
-      query.on('row', (dbUser) => users.push(this.parseChallengeAblePerson(dbUser)));
+      query.on('row', ({ challengedcount, name, spotid, spotopen }) => {
+        users.push(new ChallengeableUser(challengedcount, name, spotid, spotopen));
+      });
+
       query.on('end', () => {
         client.end();
         resolve(users);
       });
+
       query.on('error', (err) => reject(err));
     });
   }
 
-  findAllForPerformanceCSV(performanceId) {
+  static findAllForPerformanceCSV(performanceId) {
     const sql = queries.challengesForCSV;
 
     return new Promise((resolve, reject) => {
@@ -81,11 +94,31 @@ class Challenge {
 
       const query = client.query(sql, [performanceId]);
 
-      query.on('row', challenge => challenges.push(challenge));
+      query.on('row', ({
+        challengee,
+        challenger,
+        challengeespot,
+        challengerspot,
+        firstnamenumber,
+        secondnamenumber,
+        spotopen
+      }) => {
+        challenges.push(new ChallengeForCSV(
+          challengee,
+          challenger,
+          challengeespot,
+          challengerspot,
+          firstnamenumber,
+          secondnamenumber,
+          spotopen
+        ));
+      });
+
       query.on('end', () => {
         client.end();
         resolve(challenges);
       });
+
       query.on('error', err => {
         client.end();
         reject(err);
@@ -93,7 +126,7 @@ class Challenge {
     });
   }
 
-  findAllRawForPerformance(performanceId) {
+  static findAllRawForPerformance(performanceId) {
     const sql = 'SELECT * FROM challenges WHERE performanceId = $1';
 
     return new Promise((resolve, reject) => {
@@ -117,7 +150,7 @@ class Challenge {
     });
   }
 
-  findForUser(nameNumber) {
+  static findForUser(nameNumber) {
     const sql = 'SELECT * FROM challenges AS C, performances AS P WHERE userNameNumber = $1 AND C.performanceId = P.id ORDER BY C.id LIMIT 1';
 
     return new Promise((resolve, reject) => {
@@ -128,14 +161,16 @@ class Challenge {
 
       const query = client.query(sql, [nameNumber]);
 
-      query.on('row', (result) => {
+      query.on('row', ({ name, performanceid, spotid }) => {
         client.end();
-        resolve(this.parseChallenge(result));
+        resolve(new Challenge(performanceid, name, spotid));
       });
+
       query.on('end', () => {
         client.end();
         resolve(null);
       });
+
       query.on('error', (err) => {
         client.end();
         reject(err);
@@ -143,23 +178,82 @@ class Challenge {
     });
   }
 
-  parseChallenge(challenge) {
-    return {
-      performanceId: challenge.performanceid,
-      spotId: challenge.spotid,
-      performanceName: challenge.name
-    };
+}
+
+class ChallengeForCSV {
+
+  constructor(challengee, challenger, challengeeSpot, challengerSpot, firstNameNumber, secondNameNumber, spotOpen) {
+    this._challengee = challengee;
+    this._challenger = challenger;
+    this._challengeeSpot = challengeeSpot;
+    this._challengerSpot = challengerSpot;
+    this._firstNameNumber = firstNameNumber;
+    this._secondNameNumber = secondNameNumber;
+    this._spotOpen = spotOpen;
   }
 
-  parseChallengeAblePerson(challenge) {
-    return {
-      challengedCount: challenge.challengedcount,
-      challengeFull: challenge.open ? challenge.challengedcount === 2 : challenge.challengedcount === 1,
-      name: challenge.name,
-      spotOpen: challenge.open,
-      spotId: challenge.spotid
-    };
+  get challengee() {
+    return this._challengee;
   }
+
+  get challenger() {
+    return this._challenger;
+  }
+
+  get challengeeSpot() {
+    return this._challengeeSpot;
+  }
+
+  get challengerSpot() {
+    return this._challengerSpot;
+  }
+
+  get firstNameNumber() {
+    return this._firstNameNumber;
+  }
+
+  get secondNameNumber() {
+    return this._secondNameNumber;
+  }
+
+  get spotId() {
+    return this._spotId;
+  }
+
+  get spotOpen() {
+    return this._spotOpen;
+  }
+}
+
+class ChallengeableUser {
+
+  constructor(challengedCount, name, spotId, spotOpen) {
+    this._challengedCount = challengedCount;
+    this._name = name;
+    this._spotId = spotId;
+    this._spotOpen = spotOpen;
+  }
+
+  get challengedFull() {
+    return this._spotOpen ? this._challengedCount >= 2 : this._challengedCount >= 1;
+  }
+
+  get challengedCount() {
+    return this._challengedCount;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get spotId() {
+    return this._spotId;
+  }
+
+  get spotOpen() {
+    return this._spotOpen;
+  }
+
 }
 
 module.exports = Challenge;
