51b68bef5c9416ed4fb31ce9b018946792a4aa5c
Alex Tareshawty <atareshawty@gmail.com>
2016-09-03 19:21:28 -0400

    Add email to fake users

--- spec/fixtures/utils/index.js
+++ spec/fixtures/utils/index.js
@@ -20,7 +20,7 @@
 //       UserObj.nameNumber = e[4];
 //       UserObj.role = e[5];
 //       UserObj.password = bcrypt.hashSync(e[6], bcrypt.genSaltSync(1)); // eslint-disable-line no-sync
-//       UserObj.email = 'osumbit@gmail.com';
+//       UserObj.email = e[7];
 //       userArr.push(UserObj);
 //     }
 //   });
