d9ff5f8594ce7492bb87d7a24c791cdb992c0b62
Alex Tareshawty <atareshawty@gmail.com>
2016-09-04 00:27:32 -0400

    Use pool.query in Challenge model

--- models/Challenge.js
+++ models/Challenge.js
@@ -1,5 +1,5 @@
 const queries = require('../db/queries');
-const utils = require('../utils');
+const { db } = require('../utils');
 
 const attributes = ['id', 'performanceId', 'userNameNumber', 'spotId'];
 
@@ -24,156 +24,55 @@ class Challenge {
   }
 
   static create(userId, spotId, performanceId) {
-    const sql = 'SELECT make_challenge($1, $2, $3)';
-
     return new Promise((resolve, reject) => {
-      const client = utils.db.createClient();
-      let returnCode;
+      const sql = 'SELECT make_challenge($1, $2, $3)';
 
-      client.connect();
-      client.on('error', (err) => reject(err));
-
-      const query = client.query(sql, [userId, performanceId, spotId]);
-
-      query.on('row', ({ make_challenge }) => {
-        returnCode = parseInt(make_challenge, 10);
-      });
-
-      query.on('end', () => {
-        client.end();
-        resolve(returnCode);
-      });
-
-      query.on('error', (err) => {
-        client.end();
-        reject(err);
-      });
+      db.query(sql, [userId, spotId, performanceId])
+      .then(resolve)
+      .catch(reject);
     });
   }
 
   static findAllChallengeablePeopleForUser(user, performanceId) {
-    // eslint-disable-next-line
-    const sql = queries.challengeablePeople;
-
     return new Promise((resolve, reject) => {
       if (!performanceId) {
         resolve(null);
       }
+      const sql = queries.challengeablePeople;
 
-      const client = utils.db.createClient();
-      const users = [];
-
-      client.connect();
-      client.on('error', (err) => reject(err));
-
-      const query = client.query(sql, [user.instrument, user.part, user.spotId, user.nameNumber, performanceId]);
-
-      query.on('row', ({ challengedcount, name, spotid, open }) => {
-        users.push(new ChallengeableUser(challengedcount, name, spotid, open));
-      });
-
-      query.on('end', () => {
-        client.end();
-        resolve(users);
-      });
-
-      query.on('error', (err) => reject(err));
+      db.query(sql, [user.instrument, user.part, user.spotId, user.nameNumber, performanceId], instanceFromRowChallengeableUser)
+      .then(resolve)
+      .catch(reject);
     });
   }
 
   static findAllForPerformanceCSV(performanceId) {
-    const sql = queries.challengesForCSV;
-
     return new Promise((resolve, reject) => {
-      const client = utils.db.createClient();
-      const challenges = [];
+      const sql = queries.challengesForCSV;
 
-      client.connect();
-      client.on('error', err => reject(err));
-
-      const query = client.query(sql, [performanceId]);
-
-      query.on('row', ({
-        challengee,
-        challenger,
-        challengeespot,
-        challengerspot,
-        firstnamenumber,
-        secondnamenumber,
-        spotopen
-      }) => {
-        challenges.push(new ChallengeForCSV(
-          challengee,
-          challenger,
-          challengeespot,
-          challengerspot,
-          firstnamenumber,
-          secondnamenumber,
-          spotopen
-        ));
-      });
-
-      query.on('end', () => {
-        client.end();
-        resolve(challenges);
-      });
-
-      query.on('error', err => {
-        client.end();
-        reject(err);
-      });
+      db.query(sql, [performanceId], instanceFromRowChallengeForCSV)
+      .then(resolve)
+      .catch(reject);
     });
   }
 
   static findAllRawForPerformance(performanceId) {
-    const sql = 'SELECT * FROM challenges WHERE performanceId = $1';
-
     return new Promise((resolve, reject) => {
-      const client = utils.db.createClient();
-      const challenges = [];
+      const sql = 'SELECT * FROM challenges WHERE performanceId = $1';
 
-      client.connect();
-      client.on('error', err => reject(err));
-
-      const query = client.query(sql, [performanceId]);
-
-      query.on('row', challenge => challenges.push(challenge));
-      query.on('end', () => {
-        client.end();
-        resolve(challenges);
-      });
-      query.on('error', err => {
-        client.end();
-        reject(err);
-      });
+      db.query(sql, [performanceId])
+      .then(resolve)
+      .catch(reject);
     });
   }
 
   static findForUser(nameNumber) {
-    const sql = 'SELECT * FROM challenges AS C, performances AS P WHERE userNameNumber = $1 AND C.performanceId = P.id ORDER BY C.id LIMIT 1';
-
     return new Promise((resolve, reject) => {
-      const client = utils.db.createClient();
+      const sql = 'SELECT * FROM challenges AS C, performances AS P WHERE userNameNumber = $1 AND C.performanceId = P.id ORDER BY C.id LIMIT 1';
 
-      client.connect();
-      client.on('error', (err) => reject(err));
-
-      const query = client.query(sql, [nameNumber]);
-
-      query.on('row', ({ name, performanceid, spotid }) => {
-        client.end();
-        resolve(new Challenge(performanceid, name, spotid));
-      });
-
-      query.on('end', () => {
-        client.end();
-        resolve(null);
-      });
-
-      query.on('error', (err) => {
-        client.end();
-        reject(err);
-      });
+      db.query(sql, [nameNumber], instanceFromRowChallenge)
+      .then(resolve)
+      .catch(reject);
     });
   }
 
@@ -266,5 +165,14 @@ class ChallengeableUser {
   }
 
 }
+
+const instanceFromRowChallenge = ({ name, performanceid, spotid }) =>
+  new Challenge(performanceid, name, spotid);
+
+const instanceFromRowChallengeableUser = ({ challengedcount, name, spotid, open }) =>
+  new ChallengeableUser(challengedcount, name, spotid, open);
+
+const instanceFromRowChallengeForCSV = ({ challengee, challenger, challengeespot, challengerspot, firstnamenumber, secondnamenumber, spotopen }) =>
+  new ChallengeForCSV(challengee, challenger, challengeespot, challengerspot, firstnamenumber, secondnamenumber, spotopen);
 
 module.exports = Challenge;
