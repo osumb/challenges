11b320a64c79a71e187bc7a275b0c6abe2a47a34
Alex Tareshawty <atareshawty@gmail.com>
2016-09-09 12:23:07 -0400

    Front end changes for updating user spot

--- public/js/users/index.js
+++ public/js/users/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable global-require */
 module.exports = {
+  indexMember: require('./index-member.js'),
   individualManage: require('./individual-manage.js'),
   manage: require('./manage.js'),
   settings: require('./settings.js')
