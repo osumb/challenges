48d31ce1b25798085915a0cafd331c104193130e
Alex Tareshawty <atareshawty@gmail.com>
2016-09-27 15:14:41 -0400

    Add clock.js to schedule cron job for challenge list exporting

---
+++ jobs/clock.js
@@ -0,0 +1,7 @@
+const schedule = require('node-schedule');
+const emailChallengeList = require('./email-challenge-list.js');
+
+// Every 5 minutes, from 8:00AM - 5:00 PM from Monday through Friday
+const cronTime = '*/5 8-17 * * MON-FRI';
+
+schedule.scheduleJob(cronTime, emailChallengeList);
