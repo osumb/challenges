462657faa914f7d97a9647286c491fce2cf0f9f8
Alex Tareshawty <atareshawty@gmail.com>
2016-09-27 20:25:26 -0400

    Change scheduling libraray

--- jobs/clock.js
+++ jobs/clock.js
@@ -1,7 +1,12 @@
-const schedule = require('node-schedule');
+const { CronJob } = require('cron');
 const emailChallengeList = require('./email-challenge-list.js');
 
 // Every 5 minutes, from 8:00AM - 5:00 PM from Monday through Friday
 const cronTime = '*/5 8-17 * * MON-FRI';
 
-schedule.scheduleJob(cronTime, emailChallengeList);
+new CronJob({
+  cronTime,
+  onTick: emailChallengeList,
+  start: true,
+  timeZone: 'Etc/UTC'
+}, emailChallengeList);
