9e41f4136f71994dc63f9ea29b83eacde3ddc749
Alex Tareshawty <atareshawty@gmail.com>
2016-09-27 20:35:28 -0400

    Try different scheduler

--- jobs/clock.js
+++ jobs/clock.js
@@ -1,12 +1,9 @@
-const { CronJob } = require('cron');
+const cron = require('node-cron');
 const emailChallengeList = require('./email-challenge-list.js');
 
 // Every 5 minutes, from 8:00AM - 5:00 PM from Monday through Friday
 const cronTime = '*/5 8-17 * * MON-FRI';
 
-new CronJob({
-  cronTime,
-  onTick: emailChallengeList,
-  start: true,
-  timeZone: 'Etc/UTC'
+cron.schedule(cronTime, () => {
+  emailChallengeList();
 });
