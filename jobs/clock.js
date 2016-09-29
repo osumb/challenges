c830d08578e7a9a1710fb13c4b99891779da2d82
Alex Tareshawty <atareshawty@gmail.com>
2016-09-29 10:05:56 -0400

    Jobs: update clock.js with UTC conversion

--- jobs/clock.js
+++ jobs/clock.js
@@ -1,8 +1,9 @@
 const cron = require('node-cron');
 const emailChallengeList = require('./email-challenge-list.js');
 
-// Every 5 minutes, from 8:00AM - 5:00 PM from Monday through Friday
-const cronTime = '*/5 8-17 * * MON-FRI';
+// Every 5 minutes, from 8:00AM - 7:00 PM ETC everyday
+// The time is in UTC though!
+const cronTime = '*/5 12-23 * * *';
 
 cron.schedule(cronTime, () => {
   emailChallengeList();
