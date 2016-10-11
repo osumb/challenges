474098a4a01050ad1838622d196b59157e3e18dd
Alex Tareshawty <atareshawty@gmail.com>
2016-10-11 11:29:36 -0400

    Config: update config.auth.secret

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ module.exports = {
   db: {
     postgres: process.env.DATABASE_URL || 'postgres://localhost:5432/challenges_dev'
   },
-  passport: {
-    secret: process.env.PASSPORT_SECRET || 'keyboard cat'
+  auth: {
+    secret: process.env.PASSPORT_SECRET || 'notMuchOfASecret'
   },
   server: {
     port: process.env.PORT || 3000
