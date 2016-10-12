1fb2a7cc43b7d0a007ec8e0bb8e6032320921152
Alex Tareshawty <atareshawty@gmail.com>
2016-10-11 22:32:07 -0400

    Controllers: add auth controller and remove static pages/sessions

---
+++ controllers/Auth.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { auth } = require('../config');
+const { User } = require('../models');
+
+class Auth {
+  static getToken({ query }, res) {
+    const { nameNumber, password } = query;
+
+    User.findByNameNumber(nameNumber)
+      .then((user) => {
+        if (!user) {
+          res.status(404).send();
+        }
+        if (!bcrypt.compareSync(password, user.password)) { // eslint-disable-line no-sync
+          res.status(404);
+        }
+
+        const userJSON = user.toJSON();
+
+        delete userJSON.password;
+
+        const token = jwt.sign(userJSON, auth.secret);
+
+        res.json({ token });
+      })
+      .catch((err) => {
+        console.log('as;dlkjasdf;lkj');
+        console.error(err);
+        res.status(500);
+      });
+  }
+}
+
+module.exports = Auth;
