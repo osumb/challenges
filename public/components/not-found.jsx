90aae6f75d49b15e79ab13fd793729a39c473edb
Alex Tareshawty <atareshawty@gmail.com>
2016-10-12 00:14:37 -0400

    Public: NotFound component with 404 image

---
+++ public/components/not-found.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Media from 'react-media';
+
+import './not-found.scss';
+
+const minWidth = 768;
+
+const NotFound = () => (
+  <div className="NotFound">
+    <Media query={{ minWidth }}>
+      {(matches) => matches ?
+        <img className="NotFound-image" src="/public/images/404.png" /> :
+        <img className="NotFound-image--mobile" src="/public/images/404.png" />
+      }
+    </Media>
+    <Link className="NotFound-link" to="/">Get me outta here!</Link>
+  </div>
+);
+
+export default NotFound;
