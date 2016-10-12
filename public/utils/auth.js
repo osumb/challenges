57e2b8c22f35974fae29e603977a9cbbbe69b1bc
Alex Tareshawty <atareshawty@gmail.com>
2016-10-11 23:10:08 -0400

    Public: make auth.js actually do real things

--- public/utils/auth.js
+++ public/utils/auth.js
@@ -1,10 +1,28 @@
-const getUser = () => {
-  return {
-    name: 'Alex Tareshawty',
-    nameNumber: 'tareshawty.3',
-    admin: false,
-    spotId: 'R1'
-  };
+import 'whatwg-fetch';
+import jwtDecode from 'jwt-decode';
+
+import { api } from '../utils';
+
+const LOCAL_STORE_STRING = 'userJWT';
+const NON_ADMIN_ROUTES = [
+
+];
+
+const authenticate = (nameNumber, password) =>
+  api.get(`/token?nameNumber=${nameNumber}&password=${password}`)
+  .then(({ token }) => {
+    localStorage.userJWT = token;
+    return;
+  });
+
+const getUser = () => localStorage[LOCAL_STORE_STRING] && jwtDecode(localStorage[LOCAL_STORE_STRING]);
+
+const isAuthenticated = () => typeof getUser() !== 'undefined';
+
+const logout = () => {
+  localStorage.removeItem(LOCAL_STORE_STRING);
 };
 
-export default { getUser };
+const userCanAccess = (pattern) => getUser().admin || NON_ADMIN_ROUTES.includes(pattern);
+
+export default { authenticate, getUser, isAuthenticated, logout, userCanAccess };
