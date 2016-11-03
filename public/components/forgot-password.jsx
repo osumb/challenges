4e2e68441623ef96dd111ec3f3ee8b4660ea08f5
Alex Tareshawty <atareshawty@gmail.com>
2016-11-02 23:06:44 -0400

    public: forgot password styling

--- public/components/forgot-password.jsx
+++ public/components/forgot-password.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import './forgot-password.scss';
 import { api, keyCodes } from '../utils';
 import Banner from './banner';
 
@@ -41,7 +42,6 @@ export default class ForgotPassword extends Component {
           emailSent: true
         });
       });
-      console.log(email.value, nameNumber.value);
     }
   }
 
@@ -55,30 +55,33 @@ export default class ForgotPassword extends Component {
     return (
       <div className="ForgotPassword">
         {badInput && <Banner message="Please enter both your dot number and email" />}
-        <div className="ForgotPassword-input">
-          <label className="ForgotPassword-inputs-label">name.#</label>
-          <input
-            autoCorrect="off"
-            autoCapitalize="off"
-            onKeyUp={this.handleEnterKey}
-            placeholder="name.#"
-            ref="nameNumber"
-            type="text"
-          />
-        </div>
-        <div className="ForgotPassword-input">
-          <label className="ForgotPassword-inputs-label">Email</label>
-          <input
-            autoCorrect="off"
-            autoCapitalize="off"
-            onKeyUp={this.handleEnterKey}
-            placeholder="email"
-            ref="email"
-            type="text"
-          />
-        </div>
-        <div className="ForgotPassword-input">
-          <button className="ForgotPassword-button" onClick={this.handleClick}>Submit</button>
+        <h3>Enter your name.number and email to recover your email</h3>
+        <div className="ForgotPassword-inputs">
+          <div className="ForgotPassword-input">
+            <label className="ForgotPassword-inputs-label">name.#</label>
+            <input
+              autoCorrect="off"
+              autoCapitalize="off"
+              onKeyUp={this.handleEnterKey}
+              placeholder="name.#"
+              ref="nameNumber"
+              type="text"
+            />
+          </div>
+          <div className="ForgotPassword-input">
+            <label className="ForgotPassword-inputs-label">Email</label>
+            <input
+              autoCorrect="off"
+              autoCapitalize="off"
+              onKeyUp={this.handleEnterKey}
+              placeholder="email"
+              ref="email"
+              type="text"
+            />
+          </div>
+          <div className="ForgotPassword-input">
+            <button className="ForgotPassword-button" onClick={this.handleClick}>Send Email</button>
+          </div>
         </div>
       </div>
     );
