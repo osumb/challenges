2d5758fca4b70173b1512809034647d6644b156c
Alex Tareshawty <atareshawty@gmail.com>
2016-11-09 14:32:50 -0500

    App: add middleware to refresh token and to send json response

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const path = require('path');
 
-const { getToken, getUserFromToken, verifyToken } = require('./auth');
+const { getToken, getUserFromToken, refreshToken, verifyToken } = require('./auth');
 const router = require('./routes/routes');
 
 const app = express();
@@ -14,12 +14,20 @@ if (process.env.NODE_ENV !== 'production') app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/dist')));
+app.use('/public/images', express.static(path.join(__dirname, '/public/images')));
+
+// This middleware verifies a possible token coming in from a request
+// If there is a valid token, add the corresponding user to req.user and refreshes expiration date on token
+// It also appends token to res.locals.token
 app.use((req, res, next) => {
   const token = getToken(req);
 
   verifyToken(token)
     .then((verified) => {
-      if (verified) req.user = getUserFromToken(token);
+      if (verified) {
+        req.user = getUserFromToken(token);
+        res.locals.token = refreshToken(req.user);
+      }
       if (!verified) req.user = {};
       next();
     })
@@ -29,9 +37,13 @@ app.use((req, res, next) => {
       next();
     });
 });
-app.use('/public/images', express.static(path.join(__dirname, '/public/images')));
 
+// Api middleware assumes appends response (if successful) to res.locals.jsonResp
+// Failed responses won't make it past this middleware
 app.use('/api', router);
+
+// After controllers attach response, combine with token and send
+app.use('/api', (req, res) => res.json(Object.assign({}, { token: res.locals.token }, res.locals.jsonResp)));
 
 app.use((req, res) => {
   res.sendFile(path.resolve(__dirname, './dist/index.html'));
