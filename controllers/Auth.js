78c75c9458657e807c74f384f650fbdfa4f67811
Alex Tareshawty <atareshawty@gmail.com>
2016-11-14 14:19:29 -0500

    Auth.js: put return back

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -12,9 +12,11 @@ class Auth {
       .then((user) => {
         if (!user) {
           res.status(404).send();
+          return;
         }
         if (!bcrypt.compareSync(password, user.password)) { // eslint-disable-line no-sync
           res.status(404).send();
+          return;
         }
         res.json({ token: tokenFromUser(user.toJSON()) });
       })
