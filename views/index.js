8404f8e438c318fa8c5cbc4427e8e54d84256605
Alex Tareshawty <atareshawty@gmail.com>
2016-12-06 21:17:07 -0500

    git is weird

---
+++ views/index.js
@@ -0,0 +1,26 @@
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+
+
+module.exports = (app) => {
+  app.set('view engine', 'handlebars');
+  app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    helpers: {
+      evenOdd: (index) => index % 2 === 0 ? 'EvenRow' : 'OddRow',
+      usersIndividualManageActionItem: (name, performanceName, reason, voluntary) => {
+        let action = voluntary ? `${name} opened their spot` : 'an admin opened the spot';
+
+        if ((reason || '').toLowerCase() === 'Closed Spot'.toLowerCase()) {
+          action = 'an admin closed the spot';
+        }
+
+        const actionReason = voluntary ? '' : ` because <span style="font-weight: bold;">${reason}</span>`;
+        const message = reason ? `For the ${performanceName}, ${action} ${actionReason}` : 'No Current Action Item';
+
+        return new Handlebars.SafeString(message);
+      },
+      spotIsOpen: (id, open) => new Handlebars.SafeString(`Spot ${id} ${open ? 'is' : 'isn\'t'} open`)
+    }
+  }));
+};
