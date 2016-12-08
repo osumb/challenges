8798e29f7ee2ac73599bc70136cbf503aa9ed5cc
Alex Tareshawty <atareshawty@gmail.com>
2016-12-08 01:25:55 -0500

    move auth/ into a util and add back some old utils

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,11 @@
-module.exports = {
+const auth = require('./auth');
+const db = require('./db');
+const identityFunction = require('./identity-function');
+const logger = require('./logger');
 
+module.exports = {
+  auth,
+  db,
+  identityFunction,
+  logger
 };
---
+++ utils/logger.js
@@ -0,0 +1,36 @@
+// const email = require('./email.js');
+
+const actionLog = (message, obj = '') => {
+  console.log(`ACTION_LOG: ${message}`, obj);
+};
+
+const adminActionLog = (message, obj = '') => {
+  console.log(`ADMIN_ACTION_LOG: ${message}`, obj);
+};
+
+const challengesLog = (message, obj = '') => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(`CHALLENGES_LOG: ${message}`, obj);
+  }
+};
+
+const errorLog = (message, err = '') => {
+  // if (process.env.NODE_ENV && process.env.NODE_ENV !== 'dev' && process.env.NODE_ENV !== 'test') {
+  //   email.sendErrorEmail(message);
+  // }
+  console.error(`ERROR_LOG: ${message}`, err);
+};
+
+const jobsLog = (message, obj = '') => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(`JOBS_LOG: ${message}`, obj);
+  }
+};
+
+module.exports = {
+  actionLog,
+  adminActionLog,
+  challengesLog,
+  errorLog,
+  jobsLog
+};
