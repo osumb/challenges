22e657f0bab3892a9c50fb81fb94ad785d4dc54f
Alex Tareshawty <atareshawty@gmail.com>
2016-12-13 19:49:44 -0500

    spec: make consistent and better seed data

--- spec/fixtures/mock-data/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  fakeChallengeList: require('./fakeChallengeList.js'),
-  fakePerformances: require('./fakePerformances.js'),
-  fakeResults: require('./fakeResults.js'),
-  fakeResultsApprove: require('./fakeResultPermission.js'),
-  fakeSpots: require('./fakeSpots.js'),
-  fakeUsers: require('./fakeUsers.js')
-};
