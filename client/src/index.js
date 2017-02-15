8bb466325a294cd4d6ba9a67fa76f5bb3dbb9503
Alex Tareshawty <atareshawty@gmail.com>
2017-02-15 01:47:56 -0500

    WE NOW HAVE HOT MODULE RELOADING!!!

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -7,3 +8,14 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+
+    ReactDOM.render(
+      <NextApp />,
+      document.getElementById('root')
+    );
+  });
+}
