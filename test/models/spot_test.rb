fbc1a6466c5493492c3f48f212b80b771ecce30a
Alex Tareshawty <atareshawty@gmail.com>
2017-02-20 21:41:26 -0500

    rails: refactor spot_test

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -12,14 +12,18 @@ class SpotTest < ActiveSupport::TestCase
     refute spot.valid?
   end
 
-  test 'a and x rows should allow efer, solo, first trumpet parts' do
+  test 'a and x rows should only allow efer, solo, first trumpet parts' do
     a_row = Spot.rows[:a]
     x_row = Spot.rows[:x]
     instrument = User.instruments[:trumpet]
     parts = [User.parts[:efer], User.parts[:solo], User.parts[:first]]
-    parts.each do |part|
-      assert Spot.valid_instrument_part_for_row(a_row, instrument, part)
-      assert Spot.valid_instrument_part_for_row(x_row, instrument, part)
+    select_user_parts(parts) do |_key, value|
+      assert Spot.valid_instrument_part_for_row(a_row, instrument, value)
+      assert Spot.valid_instrument_part_for_row(x_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(a_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(x_row, instrument, value)
     end
   end
 
@@ -34,27 +38,18 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'a and x rows shouldn\'t allow non efer, solo, first parts' do
-    a_row = Spot.rows[:a]
-    x_row = Spot.rows[:x]
-    instruments = User.instruments[:trumpet]
-    parts = User.parts.select do |p|
-      ![User.parts[:efer], User.parts[:solo], User.parts[:first]].include? User.parts[p]
-    end
-    parts.each do |_key, p|
-      refute Spot.valid_instrument_part_for_row(a_row, instruments, p)
-      refute Spot.valid_instrument_part_for_row(x_row, instruments, p)
-    end
-  end
-
-  test 'b and t rows should allow first, second trumpet parts' do
+  test 'b and t rows should only allow first, second trumpet parts' do
     b_row = Spot.rows[:b]
     t_row = Spot.rows[:t]
     instrument = User.instruments[:trumpet]
     parts = [User.parts[:first], User.parts[:second]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(b_row, instrument, value)
       assert Spot.valid_instrument_part_for_row(t_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(b_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(t_row, instrument, value)
     end
   end
 
@@ -69,25 +64,15 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'b and t rows shouldn\'t allow non first, second parts' do
-    b_row = Spot.rows[:b]
-    t_row = Spot.rows[:t]
-    instruments = User.instruments[:trumpet]
-    parts = User.parts.select do |p|
-      ![User.parts[:first], User.parts[:second]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(b_row, instruments, value)
-      refute Spot.valid_instrument_part_for_row(t_row, instruments, value)
-    end
-  end
-
-  test 'c row should allow flugel part' do
+  test 'c row should allow only flugel part' do
     c_row = Spot.rows[:c]
     instrument = User.instruments[:trumpet]
     parts = [User.parts[:flugel]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(c_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(c_row, instrument, value)
     end
   end
 
@@ -100,25 +85,18 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'c row shouldn\'t allow non flugel parts' do
-    c_row = Spot.rows[:c]
-    instruments = User.instruments[:trumpet]
-    parts = User.parts.select do |p|
-      ![User.parts[:flugel]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(c_row, instruments, value)
-    end
-  end
-
-  test 'e and r rows should allow first, second mellophone parts' do
+  test 'e and r rows should only allow first, second mellophone parts' do
     e_row = Spot.rows[:e]
     r_row = Spot.rows[:r]
     instrument = User.instruments[:mellophone]
     parts = [User.parts[:first], User.parts[:second]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(e_row, instrument, value)
       assert Spot.valid_instrument_part_for_row(r_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(e_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(r_row, instrument, value)
     end
   end
 
@@ -133,27 +111,18 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'e and r rows shouldn\'t allow non first, second parts' do
-    e_row = Spot.rows[:e]
-    r_row = Spot.rows[:r]
-    instruments = User.instruments[:mellophone]
-    parts = User.parts.select do |p|
-      ![User.parts[:first], User.parts[:second]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(e_row, instruments, value)
-      refute Spot.valid_instrument_part_for_row(r_row, instruments, value)
-    end
-  end
-
-  test 'f and q rows should allow first, second, and bass trombone parts' do
+  test 'f and q rows should only allow first, second, and bass trombone parts' do
     f_row = Spot.rows[:f]
     q_row = Spot.rows[:q]
     instrument = User.instruments[:trombone]
     parts = [User.parts[:first], User.parts[:second], User.parts[:bass]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(f_row, instrument, value)
       assert Spot.valid_instrument_part_for_row(q_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(f_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(q_row, instrument, value)
     end
   end
 
@@ -168,27 +137,18 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'f and q rows shouldn\'t allow non first, second, and bass parts' do
-    f_row = Spot.rows[:f]
-    q_row = Spot.rows[:q]
-    instruments = User.instruments[:trombone]
-    parts = User.parts.select do |p|
-      ![User.parts[:first], User.parts[:second], User.parts[:bass]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(f_row, instruments, value)
-      refute Spot.valid_instrument_part_for_row(q_row, instruments, value)
-    end
-  end
-
-  test 'h and m rows should allow the first baritone part' do
+  test 'h and m rows should only allow the first baritone part' do
     h_row = Spot.rows[:h]
     m_row = Spot.rows[:m]
     instrument = User.instruments[:baritone]
     parts = [User.parts[:first]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(h_row, instrument, value)
       assert Spot.valid_instrument_part_for_row(m_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(h_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(m_row, instrument, value)
     end
   end
 
@@ -203,25 +163,15 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'h and m rows shouldn\'t allow non first parts' do
-    h_row = Spot.rows[:h]
-    m_row = Spot.rows[:m]
-    instruments = User.instruments[:baritone]
-    parts = User.parts.select do |p|
-      ![User.parts[:first]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(h_row, instruments, value)
-      refute Spot.valid_instrument_part_for_row(m_row, instruments, value)
-    end
-  end
-
   test 'i row should allow snare part' do
     i_row = Spot.rows[:i]
     instrument = User.instruments[:percussion]
     parts = [User.parts[:snare]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(i_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(i_row, instrument, value)
     end
   end
 
@@ -234,23 +184,15 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'i row shouldn\'t allow non snare parts' do
-    i_row = Spot.rows[:i]
-    instruments = User.instruments[:percussion]
-    parts = User.parts.select do |p|
-      ![User.parts[:snare]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(i_row, instruments, value)
-    end
-  end
-
-  test 'j row should allow bass, cymbals, and tenor parts' do
+  test 'j row should allow only bass, cymbals, and tenor parts' do
     j_row = Spot.rows[:j]
     instrument = User.instruments[:percussion]
     parts = [User.parts[:bass], User.parts[:cymbals], User.parts[:tenor]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(j_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(j_row, instrument, value)
     end
   end
 
@@ -263,25 +205,18 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'j row shouldn\'t allow non bass, cymbals, and tenor parts' do
-    j_row = Spot.rows[:j]
-    instruments = User.instruments[:percussion]
-    parts = User.parts.select do |p|
-      ![User.parts[:bass], User.parts[:cymbals], User.parts[:tenor]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(j_row, instruments, value)
-    end
-  end
-
-  test 'k and l rows should allow the first baritone part' do
+  test 'k and l rows should only allow the first baritone part' do
     k_row = Spot.rows[:k]
     l_row = Spot.rows[:l]
     instrument = User.instruments[:sousaphone]
     parts = [User.parts[:first]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(k_row, instrument, value)
       assert Spot.valid_instrument_part_for_row(l_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(k_row, instrument, value)
+      refute Spot.valid_instrument_part_for_row(l_row, instrument, value)
     end
   end
 
@@ -296,25 +231,15 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 'k and l rows shouldn\'t allow non first parts' do
-    k_row = Spot.rows[:k]
-    l_row = Spot.rows[:l]
-    instruments = User.instruments[:sousaphone]
-    parts = User.parts.select do |p|
-      ![User.parts[:first]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(k_row, instruments, value)
-      refute Spot.valid_instrument_part_for_row(l_row, instruments, value)
-    end
-  end
-
-  test 's row should allow second and flugel parts' do
+  test 's row should only allow second and flugel parts' do
     s_row = Spot.rows[:s]
     instrument = User.instruments[:trumpet]
     parts = [User.parts[:second], User.parts[:flugel]]
-    parts.each do |value|
+    select_user_parts(parts) do |_key, value|
       assert Spot.valid_instrument_part_for_row(s_row, instrument, value)
+    end
+    reject_user_parts(parts) do |_key, value|
+      refute Spot.valid_instrument_part_for_row(s_row, instrument, value)
     end
   end
 
@@ -327,14 +252,13 @@ class SpotTest < ActiveSupport::TestCase
     end
   end
 
-  test 's row shouldn\'t allow non second or flugel parts' do
-    s_row = Spot.rows[:s]
-    instruments = User.instruments[:trumpet]
-    parts = User.parts.select do |p|
-      ![User.parts[:second], User.parts[:flugel]].include? User.parts[p]
-    end
-    parts.each do |_key, value|
-      refute Spot.valid_instrument_part_for_row(s_row, instruments, value)
-    end
+  private
+
+  def select_user_parts(parts)
+    User.parts.select { |_key, value| parts.include? value }.each { |k, v| yield k, v }
+  end
+
+  def reject_user_parts(parts)
+    User.parts.reject { |_key, value| parts.include? value }.each { |k, v| yield k, v }
   end
 end
