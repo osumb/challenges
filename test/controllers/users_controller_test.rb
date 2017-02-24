c56d97bc50d3af5139d23f77af8c8a0acf40093c
Alex Tareshawty <atareshawty@gmail.com>
2017-02-23 23:50:58 -0500

    rails: fix controller test name

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'GET #index returns an array' do
+  test 'GET #index returns a 200' do
     response = get users_url(format: :json)
     assert_equal 200, response
   end
