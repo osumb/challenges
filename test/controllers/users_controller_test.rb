ca671493491f2bff33822069da2b56738cc25acb
Alex Tareshawty <atareshawty@gmail.com>
2017-02-28 23:29:02 -0500

    Rails: remove broken controller test because it wasn't doing anything anyway

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'GET #index returns a 200' do
-    response = get users_url(format: :json)
-    assert_equal 200, response
-  end
 end
