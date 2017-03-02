5519944c480c03fdf8deaa6f3281b507ee41712e
Alex Tareshawty <atareshawty@gmail.com>
2017-03-01 23:24:59 -0500

    Rails: add user show route and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user
-  before_action :ensure_admin!
+  before_action :ensure_admin!, except: [:show]
 
   def index
     @users = User.performers.includes(:spot)
+  end
+
+  def show
+    @user = User.includes(:spot, :challenges, :disciplines).find_by(buck_id: params[:id])
+    return unless @user.nil?
+    head 404
   end
 
   private
