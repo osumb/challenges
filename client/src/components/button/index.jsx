300086e375d29db3a7c81b497ff13fbc01a1a4f6
Alex Tareshawty <atareshawty@gmail.com>
2017-03-03 00:57:23 -0500

    React: add base.scss with button component using material-design-components web

---
+++ client/src/components/button/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import classNames from 'classnames';
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple.min.js';
+
+import './index.scss';
+export default class Button extends React.PureComponent {
+  componentDidMount() {
+    MDCRipple.attachTo(this.button);
+  }
+
+  render() {
+    const { children, className, onClick, ...rest } = this.props;
+    const classes = classNames({
+      [className]: Boolean(className),
+      'mdc-button': true,
+      'mdc-button--raised': Boolean(rest.raised),
+      'mdc-button--dense': Boolean(rest.dense),
+      'mdc-button--compact': Boolean(rest.compact),
+      'mdc-button--primary': Boolean(rest.primary),
+      'mdc-button--accent': Boolean(rest.accent)
+    });
+
+    return (
+      <button
+        className={classes}
+        onClick={onClick}
+        ref={ref => {
+          this.button = ref;
+        }}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  children: React.PropTypes.node,
+  className: React.PropTypes.string,
+  onClick: React.PropTypes.func
+};
