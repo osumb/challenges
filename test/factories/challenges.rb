47e8175b90cdba4dcdf0b50f2145601a38d881de
Alex Tareshawty <atareshawty@gmail.com>
2017-03-22 22:46:09 -0400

    fix bug in challenges factory for CI

--- test/factories/challenges.rb
+++ test/factories/challenges.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :challenge do
     performance { Performance.first || FactoryGirl.create(:performance) }
+    spot { Spot.find_by(row: Spot.rows[:a], file: 1) || FactoryGirl.create(:spot) }
   end
 
   factory :open_spot_challenge, parent: :challenge do
