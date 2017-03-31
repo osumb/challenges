58858e9c3248fff79d4051defaf0d39c762d99e4
Alex Tareshawty <atareshawty@gmail.com>
2017-03-30 20:49:55 -0400

    React: rename identity function to noOP

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,9 +3,9 @@ import auth from './auth';
 import changeCase from './change_case';
 import compareSpots from './compare-spots';
 import errorEmitter from './error-emitter';
-import identityFunction from './identity-function';
 import isEmptyObject from './is_empty_object';
+import noOp from './no_op';
 import * as routes from './routes';
 import screenSizes from './screen-sizes';
 
-export { api, auth, changeCase, compareSpots, errorEmitter, identityFunction, isEmptyObject, routes, screenSizes };
+export { api, auth, changeCase, compareSpots, errorEmitter, isEmptyObject, noOp, routes, screenSizes };
