e6269a528c35743ffce22ab9b3566a27b4c47d60
Alex Tareshawty <atareshawty@gmail.com>
2017-05-04 21:55:33 -0400

    Rails: actually send email for password reset

--- app/controllers/password_reset_requests_controller.rb
+++ app/controllers/password_reset_requests_controller.rb
@@ -10,6 +10,7 @@ class PasswordResetRequestsController < ApplicationController
     # We're reloading because prr.expires is a default value created in the db. Unfortunately, it doesn't get returned
     if @prr.save && @prr.reload
       render :create, status: 201
+      PasswordResetMailer.password_reset_email(user, @prr.id).deliver_now
     else
       render json: { resource: 'PasswordResetRequest', errors: @prr.errors }, status: 409
     end
