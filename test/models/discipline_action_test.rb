b83b27f7cebda84ddf095ca3bfdab0f0dcde5f61
Kyle Thompson <kyle@kylekthompson.com>
2017-05-11 19:28:52 -0400

    move discipline action model test to rspec

--- test/models/discipline_action_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DisciplineActionTest < ActiveSupport::TestCase
-  test 'is invalid without reason' do
-    action = build(:discipline_action, reason: nil)
-    refute action.valid?
-    assert action.errors.full_messages.include? 'Reason can\'t be blank'
-  end
-end
