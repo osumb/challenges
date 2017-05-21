bec215902bf0133c471d3f44aec4d804ac98e47e
Alex Tareshawty <atareshawty@gmail.com>
2017-05-20 23:00:17 -0700

    Rails: update challenge
    Create migration that fixes challenge.winner_buck_id to reflect the change in user's primary key
    Make better views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504003831) do
+ActiveRecord::Schema.define(version: 20170521053959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
