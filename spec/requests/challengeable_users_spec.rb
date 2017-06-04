4331645c945e3836385d6c491eceea7636be0182
Alex Tareshawty <atareshawty@gmail.com>
2017-06-04 16:08:30 -0700

    Rails: add ability for admin to query challengeable users on behalf of user

--- spec/requests/challengeable_users_spec.rb
+++ spec/requests/challengeable_users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Challengeable Users', type: :request do
-  let(:endpoint) { '/api/performances/challengeable_users/' }
+  let(:endpoint) { '/api/performances/challengeable_users' }
   let!(:performance) { create(:performance) }
   let!(:user) { create(:alternate_user, :trumpet, :solo, :spot_a13) }
   let!(:challengee) { create(:user, :trumpet, :solo, :spot_a2) }
@@ -30,38 +30,61 @@ describe 'Challengeable Users', type: :request do
   end
 
   describe 'GET /api/performances/challengeable_users/' do
-    before do
-      get endpoint, headers: authenticated_header(user)
-    end
-
-    specify { expect(response).to have_http_status(200) }
-
-    context 'when there is a current performance' do
-      it 'returns an array of challengeable users' do
-        expect(JSON.parse(response.body)['challengeable_users']).to be_instance_of(Array)
+    context 'when the requesting user isn\'t an admin' do
+      before do
+        get endpoint, headers: authenticated_header(user)
       end
 
-      it 'returns the correct shape of data' do
-        challengeable_user = JSON.parse(response.body)['challengeable_users'].first
-        expect(challengeable_user).to be_shape_of(challengeable_user_shape)
+      specify { expect(response).to have_http_status(200) }
+
+      context 'when there is a current performance' do
+        it 'returns an array of challengeable users' do
+          expect(JSON.parse(response.body)['challengeable_users']).to be_instance_of(Array)
+        end
+
+        it 'returns the correct shape of data' do
+          challengeable_user = JSON.parse(response.body)['challengeable_users'].first
+          expect(challengeable_user).to be_shape_of(challengeable_user_shape)
+        end
       end
-    end
 
-    context 'when there is no current performance' do
-      let!(:performance) { nil }
+      context 'when there is no current performance' do
+        let!(:performance) { nil }
 
-      it 'returns an empty array' do
-        challengeable_users = JSON.parse(response.body)['challengeable_users']
-        expect(challengeable_users.length).to eq(0)
+        it 'returns an empty array' do
+          challengeable_users = JSON.parse(response.body)['challengeable_users']
+          expect(challengeable_users.length).to eq(0)
+        end
       end
     end
 
     context 'when the requesting user is an admin' do
-      let(:user) { create(:admin_user, :trumpet, :solo) }
+      let(:admin) { create(:admin_user) }
 
-      it 'returns an empty array' do
-        challengeable_users = JSON.parse(response.body)['challengeable_users']
-        expect(challengeable_users.length).to eq(0)
+      before do
+        get "#{endpoint}?user_buck_id=#{user.buck_id}", headers: authenticated_header(admin)
+      end
+
+      specify { expect(response).to have_http_status(200) }
+
+      context 'when there is a current performance' do
+        it 'returns an array of challengeable users' do
+          expect(JSON.parse(response.body)['challengeable_users']).to be_instance_of(Array)
+        end
+
+        it 'returns the correct shape of data' do
+          challengeable_user = JSON.parse(response.body)['challengeable_users'].first
+          expect(challengeable_user).to be_shape_of(challengeable_user_shape)
+        end
+      end
+
+      context 'when there is no current performance' do
+        let!(:performance) { nil }
+
+        it 'returns an empty array' do
+          challengeable_users = JSON.parse(response.body)['challengeable_users']
+          expect(challengeable_users.length).to eq(0)
+        end
       end
     end
   end
