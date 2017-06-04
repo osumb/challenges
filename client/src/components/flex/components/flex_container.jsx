7d17a48bfe1eef3843f129dd7a8f6b70bf79abd2
Alex Tareshawty <atareshawty@gmail.com>
2017-06-03 22:17:21 -0700

    React: add flex container component and checkbox component

---
+++ client/src/components/flex/components/flex_container.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  display: flex;
+  ${({ margin }) => margin && `margin: ${margin};`};
+  ${({ padding }) => padding && `padding: ${padding};`};
+  ${({ height }) => height && `height: ${height};`};
+  ${({ width }) => width && `width: ${width};`};
+  ${({ flexDirection }) => flexDirection && `flex-direction: ${flexDirection};`};
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems};`};
+  ${({ justifyContent }) => justifyContent && `justify-content: ${justifyContent};`};
+  ${({ flexWrap }) => flexWrap && `flex-wrap: ${flexWrap};`};
+`;
