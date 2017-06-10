a608d5be6342d45dc5c13d25928b508f931b3589
Alex Tareshawty <atareshawty@gmail.com>
2017-06-09 22:18:27 -0700

    React: refactor create performance and have performance index component

---
+++ client/src/components/performance/index.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import Datetime from 'react-datetime';
+
+import './index.scss';
+import '../../../../node_modules/react-datetime/css/react-datetime.css';
+import { helpers as performanceHelpers, propTypes as performanceProps } from '../../data/performance';
+import { errorEmitter } from '../../utils';
+import { FlexContainer } from '../flex';
+import Button from '../button';
+import TextField from '../textfield';
+import Typography from '../typography';
+
+export default class Performance extends React.PureComponent {
+  static get propTypes() {
+    return {
+      buttonText: React.PropTypes.string.isRequired,
+      onAction: React.PropTypes.func.isRequired,
+      performance: React.PropTypes.shape(performanceProps.performance)
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props.performance
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleWindowCloseChange = this.handleWindowCloseChange.bind(this);
+    this.handleWindowOpenChange = this.handleWindowOpenChange.bind(this);
+  }
+
+  handleClick() {
+    const performance = this.state;
+    if (performanceHelpers.isValidPerformance(performance)) {
+      this.props.onAction(performance);
+    } else {
+      const errorMessage = performanceHelpers.performanceErrors(performance).join('; ');
+      errorEmitter.dispatch(errorMessage);
+    }
+  }
+
+  handleDateChange(date) {
+    this.setState({ date });
+  }
+
+  handleNameChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
+  handleWindowCloseChange(windowClose) {
+    this.setState({ windowClose });
+  }
+
+  handleWindowOpenChange(windowOpen) {
+    this.setState({ windowOpen });
+  }
+
+  render() {
+    const { date, name, windowClose, windowOpen } = this.state;
+    const { buttonText } = this.props;
+
+    return (
+      <FlexContainer flexDirection="column" alignItems="center" justifyContent="center" padding="20px">
+        <FlexContainer flexDirection="column" alignItems="flex-start">
+          <FlexContainer flexDirection="column" alignItems="flex-start" margin="5px 0">
+            <TextField onChange={this.handleNameChange} placeholder="Performance Name" name="name" value={name || ''} />
+          </FlexContainer>
+          <FlexContainer flexDirection="column" alignItems="flex-start" margin="5px 0">
+            <Typography category="title">Date of performance</Typography>
+            <Datetime
+              className="Challenges-Datetime"
+              onChange={this.handleDateChange}
+              value={date && new Date(date)}
+            />
+          </FlexContainer>
+          <FlexContainer flexDirection="column" alignItems="flex-start" margin="5px 0">
+            <Typography category="title">Challenge Window Open</Typography>
+            <Datetime
+              className="Challenges-Datetime"
+              onChange={this.handleWindowOpenChange}
+              value={windowOpen && new Date(windowOpen)}
+            />
+          </FlexContainer>
+          <FlexContainer flexDirection="column" alignItems="flex-start" margin="5px 0">
+            <Typography category="title">Challenge Window Close</Typography>
+            <Datetime
+              className="Challenges-Datetime"
+              onChange={this.handleWindowCloseChange}
+              value={windowClose && new Date(windowClose)}
+            />
+          </FlexContainer>
+          <Button onClick={this.handleClick}>{buttonText}</Button>
+        </FlexContainer>
+      </FlexContainer>
+    );
+  }
+}
---
+++ client/src/scenes/performance/index/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import moment from 'moment';
+
+import { helpers, propTypes } from '../../../data/performance';
+import { fetch } from '../../../utils';
+import { FlexContainer, FlexChild } from '../../../components/flex';
+import Elevation from '../../../components/elevation';
+import Performance from '../../../components/performance';
+import Snackbar from '../../../components/snackbar';
+import Typography from '../../../components/typography';
+
+const formatString = 'MM/DD/YYYY hh:mm A';
+
+class PerformanceIndex extends React.PureComponent {
+  static get propTypes() {
+    return {
+      performances: React.PropTypes.arrayOf(React.PropTypes.shape(propTypes.performance)).isRequired
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      performancesById: props.performances.reduce((acc, { id, date, windowClose, windowOpen, ...rest }) => {
+        acc[id] = {
+          ...rest,
+          id,
+          date: moment(new Date(date)).format(formatString),
+          windowClose: moment(new Date(windowClose)).format(formatString),
+          windowOpen: moment(new Date(windowOpen)).format(formatString)
+        };
+
+        return acc;
+      }, {}),
+      updated: false
+    };
+    this.handlePerformanceUpdate = this.handlePerformanceUpdate.bind(this);
+  }
+
+  handlePerformanceUpdate({ id, ...rest }) {
+    this.setState({ updated: false });
+    helpers.update({ id, ...rest })
+    .then(() => {
+      this.setState(({ performancesById }) => ({
+        performancesById: {
+          ...performancesById,
+          [id]: {
+            id,
+            ...rest
+          }
+        },
+        updated: true
+      }));
+    });
+  }
+
+  render() {
+    const { performancesById, updated } = this.state;
+
+    return (
+      <FlexContainer flexDirection="column" alignItems="center" margin="20px 100px">
+        <Typography category="display" number={2}>Update Performances</Typography>
+        <FlexChild flex={1} padding="20px 0 0 0" width="100%">
+          <FlexContainer flexWrap="wrap" justifyContent="space-between" alignItems="center" flex={1}>
+            {Object.keys(performancesById).sort().map(id => (
+              <Elevation key={id}>
+                <Performance
+                  buttonText="Update"
+                  onAction={this.handlePerformanceUpdate}
+                  performance={performancesById[id]}
+                />
+              </Elevation>
+            ))}
+          </FlexContainer>
+        </FlexChild>
+        <Snackbar show={updated} message="Updated Performance" />
+      </FlexContainer>
+    );
+  }
+}
+
+export default fetch(helpers.getAll, null, PerformanceIndex);
