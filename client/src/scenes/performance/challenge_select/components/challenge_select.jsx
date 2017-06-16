54b5c68ae1c56b8f9dd4dc0cf57066573e199e8b
Alex Tareshawty <atareshawty@gmail.com>
2017-06-15 21:28:46 -0700

    React: create UI to allow admin to make challenge on behalf of user

---
+++ client/src/scenes/performance/challenge_select/components/challenge_select.jsx
@@ -0,0 +1,202 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { auth } from '../../../../utils';
+import { propTypes as performanceProps } from '../../../../data/performance';
+import { helpers as challengeHelpers } from '../../../../data/challenge';
+import {
+  helpers as userHelpers,
+  propTypes as userProps
+} from '../../../../data/user';
+import { FlexContainer } from '../../../../components/flex';
+import Button from '../../../../components/button';
+import NormalChallengeableUser from './normal_challengeable_user';
+import OpenSpotChallengeableUser from './open_spot_challengeable_user';
+import TriChallengeableUser from './tri_challengeable_user';
+import Typography from '../../../../components/typography';
+import Select from '../../../../components/select';
+
+const ButtonWrapper = styled.div`
+  margin-left: 25px;
+  margin-top: 10px;
+`;
+const ErrorText = styled.div`
+  color: red;
+`;
+
+export default class ChallengeSelect extends React.PureComponent {
+  static get propTypes() {
+    return {
+      challengeableUsers: React.PropTypes.arrayOf(
+        React.PropTypes.shape(performanceProps.challengeableUser)
+      ),
+      onChallenge: React.PropTypes.func,
+      performance: React.PropTypes.shape(performanceProps.performance),
+      user: React.PropTypes.shape(userProps)
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      successMessage: null
+    };
+    this.createChallenge = this.createChallenge.bind(this);
+    this.getUser = this.getUser.bind(this);
+    this.handleChallengeCreate = this.handleChallengeCreate.bind(this);
+    this.handleSelectClick = this.handleSelectClick.bind(this);
+  }
+
+  handleChallengeCreate(spot) {
+    const spotStr = `${spot.row}${spot.file}`;
+    const s = `Successfully challenged ${spotStr} for the ${this.props
+      .performance.name}!`;
+
+    this.setState({
+      errorMessage: null,
+      successMessage: s
+    });
+    if (this.props.onChallenge) {
+      this.props.onChallenge();
+    }
+  }
+
+  handleSelectClick() {
+    const challengeableUser = this.props.challengeableUsers[
+      this.select.getSelectedIndex()
+    ];
+
+    if (
+      challengeableUser === null ||
+      typeof challengeableUser === 'undefined'
+    ) {
+      this.setState({ errorMessage: 'Please select someone to challenge' });
+    } else {
+      this.createChallenge(challengeableUser);
+    }
+  }
+
+  createChallenge({ file, row }) {
+    const user = this.getUser();
+
+    challengeHelpers
+      .create(
+        {
+          file,
+          row
+        },
+        user.buckId
+      )
+      .then(({ challenge }) => {
+        this.handleChallengeCreate(challenge.spot);
+      });
+  }
+
+  editCurrentChallenge(challengeableUser) {
+    const user = this.getUser();
+
+    challengeHelpers
+      .addUser(user.buckId, challengeableUser.challengeId)
+      .then(({ challengeSpot }) => {
+        this.handleChallengeCreate(challengeSpot);
+      });
+  }
+
+  getUser() {
+    return this.props.user || auth.getUser();
+  }
+
+  render() {
+    const user = this.getUser();
+    const { challengeableUsers, performance } = this.props;
+    const { errorMessage, successMessage } = this.state;
+
+    if (successMessage !== null && typeof successMessage !== 'undefined') {
+      return (
+        <FlexContainer
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography category="display" number={1}>
+            {successMessage}
+          </Typography>
+        </FlexContainer>
+      );
+    }
+
+    if (challengeableUsers.length <= 0) {
+      return (
+        <FlexContainer
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography category="display" number={1}>
+            Sorry! There is no one for you to challenge at this time
+          </Typography>
+        </FlexContainer>
+      );
+    }
+
+    return (
+      <FlexContainer
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {errorMessage &&
+          <ErrorText>
+            <Typography category="display" number={2}>
+              **{errorMessage}**
+            </Typography>
+          </ErrorText>}
+        <Typography category="display" number={1}>
+          Please make a challenge for the {performance.name}
+        </Typography>
+        <FlexContainer
+          alignItems="center"
+          justifyContent="center"
+          flexWrap="wrap"
+        >
+          <Select
+            defaultText="Select one person or spot"
+            ref={select => {
+              this.select = select;
+            }}
+          >
+            {challengeableUsers.map(challengeableUser => {
+              if (userHelpers.isTriChallengeUser(user)) {
+                return (
+                  <TriChallengeableUser
+                    key={challengeableUser.buckId}
+                    {...challengeableUser}
+                  />
+                );
+              } else if (challengeableUser.openSpot) {
+                return (
+                  <OpenSpotChallengeableUser
+                    key={challengeableUser.buckId}
+                    {...challengeableUser}
+                  />
+                );
+              } else {
+                return (
+                  <NormalChallengeableUser
+                    key={challengeableUser.buckId}
+                    {...challengeableUser}
+                  />
+                );
+              }
+            })}
+          </Select>
+          <ButtonWrapper>
+            <Button primary onClick={this.handleSelectClick}>
+              Make Challenge
+            </Button>
+          </ButtonWrapper>
+        </FlexContainer>
+      </FlexContainer>
+    );
+  }
+}
---
+++ client/src/scenes/performance/challenge_select/components/users_can_challenge.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import styled from 'styled-components';
+
+import { List, ListItem } from '../../../../components/list';
+import Textfield from '../../../../components/textfield';
+import Typography from '../../../../components/typography';
+
+const userFilter = (filter, { firstName, lastName }) => {
+  const lower = filter.toLowerCase();
+
+  return (
+    firstName.toLowerCase().includes(lower) ||
+    lastName.toLowerCase().includes(lower)
+  );
+};
+
+const Container = styled.div`
+  margin: -30px 0 0 10px;
+`;
+const ListContainer = styled.div`
+  height: 400px;
+  overflow: scroll;
+`;
+const UserContainer = styled.div`
+  width: 200px;
+  &:hover {
+    background-color: #f3f3f3;
+    cursor: pointer;
+  }
+`;
+
+export default class UsersCanChallenge extends React.PureComponent {
+  static get propTypes() {
+    return {
+      onSelect: React.PropTypes.func.isRequired,
+      selectedUserBuckId: React.PropTypes.string,
+      users: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: ''
+    };
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+  }
+
+  handleFilterChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { onSelect, selectedUserBuckId, users } = this.props;
+    const { filter } = this.state;
+    const filteredUsers = users.filter(userFilter.bind(null, filter));
+
+    if (users.length > 0) {
+      return (
+        <Container>
+          <Textfield
+            placeholder="Filter users"
+            value={filter}
+            onChange={this.handleFilterChange}
+            name="filter"
+          />
+          <ListContainer>
+            <List>
+              {filteredUsers.map(user =>
+                <UserContainer
+                  key={user.buckId}
+                  onClick={onSelect.bind(null, user)}
+                  selected={selectedUserBuckId === user.buckId}
+                >
+                  <ListItem>
+                    <Typography category="subheading" number={2}>
+                      <b>{user.firstName} {user.lastName}</b>
+                      ({user.spot.row}{user.spot.file})
+                    </Typography>
+                  </ListItem>
+                </UserContainer>
+              )}
+            </List>
+          </ListContainer>
+        </Container>
+      );
+    }
+    return (
+      <Typography category="display" number={2}>
+        No users are currently elligible to make a challenge
+      </Typography>
+    );
+  }
+}
