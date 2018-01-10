2ebc435b95f3644f7327cec825fd2158d72df6ab
Alex Tareshawty <atareshawty@gmail.com>
2018-01-10 10:37:01 -0500

    Rails: adds testing controller for intentional errors

---
+++ app/controllers/testing_controller.rb
@@ -0,0 +1,10 @@
+class TestingController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_admin!
+
+  TestingControllerError = Class.new(StandardError)
+
+  def exception
+    raise TestingControllerError, 'Intentionally raising exception for testing purposes'
+  end
+end
