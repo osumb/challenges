502f88b2d4d00afd2e2ef66dd2c045254b271378
Alex Tareshawty <atareshawty@gmail.com>
2018-01-10 18:58:25 -0500

    React: adds sentry error handling for client

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable global-require, react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
 
 import './index.css';
 import 'material-components-web/dist/material-components-web.min.css';
 import App from './App';
+
+// Configure sentry
+Raven.config(process.env.REACT_APP_SENTRY_CLIENT_DSN).install();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
