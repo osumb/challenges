10b06ac0467a83f348f6c80b2b9d027a1caa4389
Alex Tareshawty <atareshawty@gmail.com>
2018-01-24 16:47:58 -0500

    Rails: control email sending with environment variables

--- app/mailers/challenge_list_mailer.rb
+++ app/mailers/challenge_list_mailer.rb
@@ -1,11 +1,9 @@
 class ChallengeListMailer < ApplicationMailer
   def challenge_list_email(file_stream)
-    return if Rails.env.test?
     attachments['ChallengeList.xlsx'] = {
       mime_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       content: file_stream.read
     }
-    # This env variable should be a comma separated list of email addresses
     to = ENV['CHALLENGE_LIST_RECIPIENTS'].split(',')
     mail(to: to, subject: 'Challenge List', from: 'osumbit@gmail.com')
   end
