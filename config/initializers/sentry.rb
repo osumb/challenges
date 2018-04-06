23e717dd6c9a27f29b0628eaa4a89dc25753bfed
Alex Tareshawty <atareshawty@gmail.com>
2018-04-05 20:16:29 -0400

    Don't initialize Sentry in development or test

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-unless ENV["SENTRY_SERVER_DSN"].nil?
+if !ENV["SENTRY_SERVER_DSN"].nil? && !Rails.env.production?
   Raven.configure do |c|
     c.dsn = ENV["SENTRY_SERVER_DSN"]
     c.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
