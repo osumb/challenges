9b103005aed0c949d21b4aa60bb8e4ab2943c902
Alex Tareshawty <atareshawty@gmail.com>
2018-07-04 12:29:34 -0400

    Sets information necessary for performer and admin profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,16 @@ class SessionsController < ApplicationController
 
   def new; end
 
-  def show; end
+  def show
+    return unless current_user.performer?
+    @performance = Performance.next
+    last_user_challenge = UserChallenge.where(user_buck_id: current_user.buck_id).last
+    last_challenge = last_user_challenge&.challenge
+    @current_challenge = last_challenge if can_current_user_see_challenge_in_profile?(last_challenge, @performance)
+    @current_user_challenge = last_user_challenge unless @current_challenge.nil?
+    @current_discipline_action = DisciplineAction.find_by(user: current_user, performance: @performance)
+    @challenges = current_user.challenges.where.not(id: @current_challenge&.id).select(&:done_stage?)
+  end
 
   def create
     user = AuthenticationService.authenticate_user(create_params, session)
@@ -27,5 +36,11 @@ class SessionsController < ApplicationController
 
   def create_params
     params.permit(:buck_id, :password)
+  end
+
+  def can_current_user_see_challenge_in_profile?(challenge, performance)
+    return false if challenge.nil? || performance.nil?
+    return false if challenge.performance_id != performance.id
+    challenge.spot_id != current_user.current_spot_id
   end
 end
