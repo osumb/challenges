110f2599606431be93537ee14663fdd9ae9b25b7
Alex Tareshawty <atareshawty@gmail.com>
2018-07-29 20:35:45 -0400

    Attempt to fix bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.includes(:challenges, :discipline_actions).find(params[:id])
     @performance = Performance.next
     @current_challenge = @user.challenges.where(performance: @performance).first
-    @past_challenges = @user.challenges.where.not(performance: @performance).order(id: :asc)
+    @past_challenges = @user.challenges.done.where.not(performance: @performance).order(id: :asc)
     @current_discipline_action = @user.discipline_actions.where(performance: @performance).first
     @past_discipline_actions = @user.discipline_actions.where.not(performance: @performance).order(id: :asc)
   end
