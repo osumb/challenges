2303d939e8df57b048a08aebd78a1d1d52d081a4
Alex Tareshawty <atareshawty@gmail.com>
2018-08-05 15:52:06 -0400

    Fixes email image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.action_mailer.asset_host = "http://localhost:#{ENV['REACT_APP_SERVER_PORT']}"
 end
